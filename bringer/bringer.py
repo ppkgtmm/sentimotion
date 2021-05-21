857112201f239722c7e8ffa0e00fed4b257c6e52
ppkgtmm <pkgtm1998@gmail.com>
2021-05-22 03:05:48 +0700

    done bringing and storing prediction results

--- bringer/bringer.py
+++ bringer/bringer.py
@@ -1,4 +1,5 @@
-import requests
+from requests import post
+from pymongo import MongoClient
 import json
 
 fp = open('../cleaned-msg.json', 'r')
@@ -9,9 +10,32 @@ assert data['message']
 
 batch_size = 32
 predictions = []
+bodies = [msg["body"] for msg in data["message"]]
 
-for i in range(0,len(data['message']), batch_size):
-    print(data['message'][i:i+batch_size])
-    # call api
+for i in range(0,len(bodies), batch_size):
+    result = post(
+        'http://localhost:5000/predict',
+        json={
+            "messages": bodies[i:i+batch_size]
+        }
+    )
+    predictions += result.json()["prediction"]
 
-# save predictions to json
+final_object = []
+
+for (p, message) in zip(predictions, data["message"]):
+    final_object.append({
+        "polarity": p["polarity"],
+        "emotion": p["emotion"],
+        "created": message["created"],
+        "space_id": message["space_id"],
+        "message_id": message["message_id"],
+    })
+
+client = MongoClient("mongodb://localhost:27017")
+
+db = client["project"]
+
+_ = db["predictions"].insert_many(final_object)
+
+print(db["predictions"].count_documents({}))
