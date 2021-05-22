b975603ba430eccff9e6696f19b53bbd49f43087
ppkgtmm <pkgtm1998@gmail.com>
2021-05-22 21:49:30 +0700

    edit get prediction function

--- api/src/predictions/predictions.service.ts
+++ api/src/predictions/predictions.service.ts
@@ -1,14 +1,18 @@
 import { Injectable } from '@nestjs/common';
 import { TimeRange } from '../shared/enums';
+import { Model } from 'mongoose';
+import { PredictionDocument } from '../schemas/predictions.schema';
 
 @Injectable()
 export class PredictionsService {
+  constructor(private readonly predictionModel: Model<PredictionDocument>) {}
   async getPredictions(timeRange: TimeRange) {
-    if (
-      timeRange === TimeRange.LAST_WEEK ||
-      timeRange === TimeRange.LAST_MONTH
-    ) {
+    if (timeRange === TimeRange.LAST_WEEK) {
+      // group by day of week
+    } else if (timeRange === TimeRange.LAST_MONTH) {
+      // group by week
     } else {
+      // group by month
     }
   }
 }
